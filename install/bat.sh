46a1b903c8af0ee7d14f80b8782faa8afadcf5de
Jason Fuller <JasonSFuller@gmail.com>
2023-08-24 10:18:38 -0400

    add install scripts

---
+++ install/bat.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+URL='https://github.com/sharkdp/bat/releases/download/v0.23.0/bat-v0.23.0-x86_64-unknown-linux-musl.tar.gz'
+
+################################################################################
+
+function error { echo "ERROR: $*" >&2; exit 1; }
+
+function cleanup {
+	cd || error "could not open home dir ($HOME)"
+  rm -rf "$dir"
+}
+
+################################################################################
+
+set -e
+trap cleanup INT EXIT
+dir=$(mktemp -d)
+cd "$dir" || error "could not open temp dir ($dir)"
+
+tar=$(basename "$URL")
+
+curl -sSL "$URL" -o "$tar"
+tar xf "$tar"
+
+install -m 0755 -d ~/bin
+install -m 0755 bat-*-x86_64-unknown-linux-musl/bat ~/bin/bat
+
---
+++ install/delta.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+URL='https://github.com/dandavison/delta/releases/download/0.16.5/delta-0.16.5-x86_64-unknown-linux-musl.tar.gz'
+
+################################################################################
+
+function error { echo "ERROR: $*" >&2; exit 1; }
+
+function cleanup {
+	cd || error "could not open home dir ($HOME)"
+  rm -rf "$dir"
+}
+
+################################################################################
+
+set -e
+trap cleanup INT EXIT
+dir=$(mktemp -d)
+cd "$dir" || error "could not open temp dir ($dir)"
+
+tar=$(basename "$URL")
+
+curl -sSL "$URL" -o "$tar"
+tar xf "$tar"
+
+install -m 0755 -d ~/bin
+install -m 0755 delta-0.16.5-x86_64-unknown-linux-musl/delta ~/bin/delta
+
