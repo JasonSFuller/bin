d6f02e0fdf208017e320661bcd74322d5dcd31bd
Jason Fuller <JasonSFuller@gmail.com>
2025-01-20 15:19:29 -0500

    add fzf

---
+++ install/fzf.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+URL='https://github.com/junegunn/fzf/releases/download/v0.58.0/fzf-0.58.0-linux_amd64.tar.gz'
+SHA='https://github.com/junegunn/fzf/releases/download/v0.58.0/fzf_0.58.0_checksums.txt'
+
+################################################################################
+
+function error { echo "ERROR: $*" >&2; exit 1; }
+
+function cleanup {
+  cd || error "could not open home dir ($HOME)"
+  #rm -rf "$dir"
+}
+
+################################################################################
+
+set -e
+trap cleanup INT EXIT
+dir=$(mktemp -d)
+cd "$dir" || error "could not open temp dir ($dir)"
+
+tar=$(basename "$URL")
+
+curl -sSL "$URL" -o "$tar"
+curl -sSL "$SHA" -o checksum
+grep "$tar" checksum > "${tar}.sha256"
+sha256sum -c "${tar}.sha256" || error "Invalid checksum"
+
+tar xf "$tar"
+
+install -m 0755 -d ~/bin
+install -m 0755 ./fzf ~/bin/fzf
