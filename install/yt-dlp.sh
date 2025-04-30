f3dce78a53b972fa0b3c85ed4fb8fa7ee8d4c84c
Jason Fuller <JasonSFuller@gmail.com>
2025-04-30 10:56:56 -0400

    add yt-dlp

---
+++ install/yt-dlp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function error { echo "ERROR: $*" >&2; exit 1; }
+
+cd /tmp || error 'cannot access /tmp'
+
+printf -v FILE '%q' 'yt-dlp_linux'
+URL='https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux'
+SHA='https://github.com/yt-dlp/yt-dlp/releases/latest/download/SHA2-256SUMS'
+
+curl -fsSLo "$FILE" "$URL"
+checksums=$(curl -fsSL "$SHA")
+grep "\s${FILE}\s*$" <<< "$checksums" > "$FILE.sha256"
+if ! sha256sum -c "$FILE.sha256"; then error 'checksum failed'; fi
+install -m 0755 yt-dlp_linux ~/bin/yt-dlp
+
